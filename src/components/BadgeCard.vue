c2e349d5c72d298e005d6bbc8e21403a0a984ec8
unknown <bencbchurchill@gmail.com>
2019-10-31 20:42:25 -0700

    Added styling to badge cards

--- src/components/BadgeCard.vue
+++ src/components/BadgeCard.vue
@@ -1,6 +1,11 @@
 <template>
-    <div>
-        {{ badge.title }} <br/>
+    <div class="card">
+        <div class="image-tag"> 
+            <img :src="badge.imageUri" height="50" width="50"/> 
+        </div>
+        <div class="card-content">
+            {{ badge.title }}
+        </div>
     </div>
 </template>
 
@@ -19,5 +24,23 @@ export default {
 </script>
 
 <style scoped>
-    
+.image-tag {
+    border-radius: 25px 0px 0px 25px;
+    background: rgb(34, 33, 34);
+    float: left;
+    padding: 8px;
+}
+.card-content {
+    background: rgb(156, 155, 156);
+    height: 70px;
+    overflow: hidden;
+}
+.card {
+    border-style: solid;
+    border-width: 0px 0px 2px 0px;
+    overflow: hidden;
+    border-radius: 25px 0px 0px 25px;
+    width: 75%;
+    margin:0 auto;
+}
 </style>
