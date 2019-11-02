21ef5c9166a0d24f3c09a99437bd544e509382bb
unknown <bencbchurchill@gmail.com>
2019-11-02 16:04:40 -0700

    Included Bootstrap dependency. Updated BadgeCard to use Bootstrap library

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,16 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
     <BadgeList/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 import BadgeList from './components/BadgeList.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld,
     BadgeList
   }
 }
--- src/components/BadgeCard.vue
+++ src/components/BadgeCard.vue
@@ -1,46 +1,62 @@
 <template>
-    <div class="card">
-        <div class="image-tag"> 
-            <img :src="badge.imageUri" height="50" width="50"/> 
-        </div>
-        <div class="card-content">
-            {{ badge.title }}
-        </div>
-    </div>
+    <b-container fluid>
+        <b-row>
+            <b-col sm="1"> </b-col>
+            <b-col sm="1" class="icon"> <img :src="badge.imageUri" height="50" width="50"/> </b-col>
+            <b-col sm="2" class="content"> {{ badge.title }} </b-col>
+            <b-col sm="4" class="content"> {{ badge.description }} </b-col> 
+            <b-col sm="1" class="content"> {{ threshold }} points </b-col>
+            <b-col sm="2" class="content actions"> button </b-col>
+            <b-col sm="1"> </b-col>
+        </b-row>
+    </b-container>
 </template>
 
 <script>
+import { BContainer, BRow, BCol } from 'bootstrap-vue'
 export default {
     name: 'BadgeCard',
     props: {
         id: Number
     },
+    components: {
+        BContainer,
+        BRow,
+        BCol
+    },
     computed: {
         badge: function() {
             return this.$store.getters.getBadge(this.id)
+        },
+        threshold: function() {
+            if(this.badge.canBeEarnedWithPoints) {
+                return this.badge.pointThreshold
+            } else {
+                return 'Reward Only'
+            }
         }
     }
 }
 </script>
 
 <style scoped>
-.image-tag {
+
+.icon {
     border-radius: 25px 0px 0px 25px;
     background: rgb(34, 33, 34);
     float: left;
-    padding: 8px;
+    padding: 4px;
 }
-.card-content {
-    background: rgb(156, 155, 156);
-    height: 70px;
-    overflow: hidden;
-}
-.card {
+.content {
+    background: rgb(156, 155, 156); 
     border-style: solid;
-    border-width: 0px 0px 2px 0px;
-    overflow: hidden;
-    border-radius: 25px 0px 0px 25px;
-    width: 75%;
-    margin:0 auto;
+    border-width: 0px 0px 1px 0px;
 }
+.content:hover {
+    background: rgb(209, 206, 206);
+}
+.actions {
+    border-radius: 0px 25px 25px 0px;
+}
+
 </style>
