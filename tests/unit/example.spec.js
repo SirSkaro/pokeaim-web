f19d77eb1407e190c24f5020ca2b654df72cbb08
unknown <bencbchurchill@gmail.com>
2019-11-02 19:28:15 -0700

    Removed example unit test

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,13 +1,10 @@
 import { expect } from 'chai'
 import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
+import BadgeList from '@/components/BadgeList.vue'
 
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
-    })
-    expect(wrapper.text()).to.include(msg)
-  })
-})
+// describe('BadgeList.vue', () => {
+//   it('shows the badges header', () => {
+//     const wrapper = shallowMount(BadgeList, {})
+//     expect(wrapper.text()).to.include('Badges')
+//   })
+// })
